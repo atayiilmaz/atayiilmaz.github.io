951ab6a658105a38b358accc55b49101b0292f01
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-03 21:55:22 +0300

    updated

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+/* hide navbar script */
+
+var togglebtn= document.querySelector(".togglebtn");
+var nav = document.querySelector(".navlinks");
+var links = document.querySelector(".navlinks li");
+
+
+togglebtn.addEventListener("click",function(){
+    this.classList.toggle("click");
+    nav.classList.toggle("open");
+})
+
+var typed = new Typed(".input",{
+    strings:["Software Developer", "Backend Developer","Web Developer"],
+})
